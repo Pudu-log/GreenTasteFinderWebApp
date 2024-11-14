56db183c69d6b2212ce8b1893f16c5f0a09f9f99
Koo Gyeong Rim <mxmmxqq@gmail.com>
2024-11-14 15:40:09 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/GreenTasteFinderWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTasteFinderWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenTasteFinderWebAppApplication.class, args);
+	}
+
+}

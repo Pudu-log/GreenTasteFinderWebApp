42709556af18a745b4baa2084583dbd41aa95a5c
HSH <victoryline629@naver.com>
2024-11-15 14:53:31 +0900

    myPage작업중3

--- src/main/java/com/example/demo/service/MyPageService.java
+++ src/main/java/com/example/demo/service/MyPageService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.IMyPageActDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyPageService {
-
+    @Autowired
+    IMyPageActDao myPageActDao;
 }

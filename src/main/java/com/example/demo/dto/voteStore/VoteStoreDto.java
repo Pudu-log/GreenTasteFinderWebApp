b75346d68f90c02e98590906e1daf2b85c166f52
K-SangJun <tkdwns000817@gmail.com>
2024-11-18 14:58:16 +0900

    vote page

--- src/main/java/com/example/demo/dto/voteStore/VoteStoreDto.java
+++ src/main/java/com/example/demo/dto/voteStore/VoteStoreDto.java
@@ -7,6 +7,8 @@ import java.sql.Timestamp;
 @Data
 public class VoteStoreDto {
     private String store_id;
+    private String id;
+    private String name;
     private Timestamp vote_dt;
     private String end_yn;
 }
--- src/main/resources/static/script/voteStoreFunc.js
+++ src/main/resources/static/script/voteStoreFunc.js
@@ -55,70 +55,90 @@ export let func = {
 
     set: {
 
-        storeInfo: (stores) => {
+        storeInfo: (data) => {
             let html = ``;
-            console.log(stores);
+
+            console.log(data);
+
+            let stores = data.storeList;
+            let votes = data.voteList;
+            let myPlaceId = data.myPlace;
+
             for (let store of stores) {
-                let img = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${store?.photos[0]?.photo_reference}&key=AIzaSyCd2XzSrgjduTBn27Faiz_H5pM8Xm81GoY`;
+                let img = "";
+                if (store?.photos?.length > 0){
+                    img = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${store?.photos[0]?.photo_reference}&key=AIzaSyCd2XzSrgjduTBn27Faiz_H5pM8Xm81GoY`;
+                }else{
+                    img = `static/images/noImg.png`;
+                }
                 let lat1 = store.geometry.location.lat;
                 let lng1 = store.geometry.location.lng;
                 let lat2 = 35.1596124;
                 let lng2 = 129.0601826;
 
-                console.log(lat1, lng1, lat2, lng2);
+                let myPlace = myPlaceId === store.place_id ? "on" : "";
+
                 let distance = func.get.distance(lat1, lng1, lat2, lng2)
                 html +=
-                    `<div class="store-box">
-                    <div class="store-img border-tl-10">
-                        <img class="border-tl-10" src="${img}">
-                    </div>
-                    <div class="store-wrap">
-                        <div class="store-title border-tr-10">
-                            <span class="border-tr-10">${store.name}</span>
+                    `<div class="store-box ${myPlace}" data-place_id="${store.place_id}">
+                        <div class="store-img border-tl-10">
+                            <img class="border-tl-10" src="${img}">
                         </div>
-                        <div class="store-gbn">
-                            <span>${store.types[0]}</span>
+                        <div class="store-wrap">
+                            <div class="store-title border-tr-10">
+                                <span class="border-tr-10">${store.name}</span>
+                            </div>
+                            <div class="store-gbn">
+                                <span>${store.types[0]}</span>
+                            </div>
+                            <div class="store-distance">
+                                <span>${distance}m</span>
+                            </div>
                         </div>
-                        <div class="store-distance">
-                            <span>${distance}M</span>
-                        </div>
-                    </div>
-                    <div class="store-2wrap">
-                        <div class="store-addr">
-                            <span>${store.formatted_address}</span>
-                        </div>
-                        <div class="store-vote-wrap">
-                            <span>홍길동 외 1명</span>
-                        </div>
-                `;
-                    if (store?.current_opening_hours?.open_now){
-                        html += `
-                        <div class="store-status on">
-                            <span>운영중</span>
-                        </div>
-                        `
-                    }else{
-                        html += `
-                        <div class="store-status">
-                            <span>마감</span>
-                        </div>
-                        `
+                        <div class="store-2wrap">
+                            <div class="store-addr">
+                                <span>${store.formatted_address}</span>
+                            </div>
+                            <div class="store-vote-wrap">
+                        `;
+                let cnt = 0;
+
+                for (let vote of votes) {
+                    if (vote.store_id === store.place_id) {
+                        cnt++;
                     }
-                    html += `
-                        <div class="store-vote-list">
-                            <ul>
-                                <li>홍길동</li>
-                                <li>홍길순</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길자</li>
-                                <li>홍길민</li>
-                                <li>홍홍홍</li>
+                }
+
+                html +=
+                    `<span>${cnt}명</span>
+                    </div>
+                    <div class="store-star">
+                        <span>★ ${store?.rating ?? 0}</span>
+                    </div>`;
+
+                if (store?.current_opening_hours?.open_now) {
+                    html +=
+                        `<div class="store-status border-bl-10 border-br-10 on">
+                            <span>운영중</span>
+                        </div>`;
+                } else {
+                    html +=
+                        `<div class="store-status border-bl-10 border-br-10">
+                            <span>종료</span>
+                        </div>`;
+                }
+                html +=
+                    `<div class="store-vote-list">
+                            <ul>`;
+
+                cnt = 0;
+                for (let vote of votes) {
+                    if (vote.store_id === store.place_id) {
+                        cnt ++;
+                        html += `<li>${cnt}. ${vote.name}</li>`;
+                    }
+                }
+                html += `
                             </ul>
                         </div>
                     </div>
@@ -126,6 +146,40 @@ export let func = {
             }
 
             document.querySelector(".store-content").innerHTML = html;
+        },
+        vote: (date, place_id, type) => {
+
+            let param = {};
+            let url = "/vote/" + date + "/" + place_id + "/" + type;
+
+            axios.post(url, param, {
+                "Content-Type": "application/x-www-form-urlencoded",
+            }).then(function (res) {
+                console.log(res);
+                if (res.data > -1) {
+                    func.get.storeInfo();
+                    func.set.boxStatus(place_id);
+                } else if (res.data === -1) {
+                    alert("투표중 오류가 발생하였습니다");
+                } else if (res.data === -2) {
+                    alert("12:45분이 지나 투표 할 수 없습니다.");
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
+
+        boxStatus: (place_id) => {
+            const storeBoxs = document.querySelectorAll(".store-box");
+
+            for (let storeBox of storeBoxs) {
+                if (storeBox.dataset.place_id === place_id) {
+                    storeBox.classList.add("on");
+                    storeBox.focus();
+                } else {
+                    storeBox.classList.remove("on");
+                }
+            }
         }
     }
 
